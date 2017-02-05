a67dd7ca327626d0880d4bc108630e4489aaffd2
grischenko-v <BooooBka@gmail.com>
2017-02-05 22:29:26 +0300

    first commit

--- js/calculator.js
+++ js/calculator.js
@@ -1,12 +1,37 @@
 
-var myVar = ""
-var sendValueToInput, calcResult;
-sendValueToInput = function(value) {
-  
-  myVar+=value;
- document.getElementById('input').value = myVar;
-};
-calcResult = function(){
-	alert(+myVar.charAt(0));
+var myVar = "", recentSymbol="";
+var sendValueToInput, calcResult, clean, updateResInpu, cleanLast, oneX, chageSign;
 
+sendValueToInput = function(value) {  
+  recentSymbol=value;
+  myVar+=value;
+  updateResInput()
+};
+
+calcResult = function(){
+	var pattern_nums =/\d+/g;
+	var pattern =/\+/g;
+	var nums, calc_sign;
+	var check_pattern =/\d+[-*+//]\d+/g
+	nums = myVar.match(pattern_nums);
+    calc_sign = myVar.match(pattern);
+    alert(Number(nums[0]) + Number(nums[1]));
+};
+clean = function(){
+	myVar="";
+	updateResInput()
+};
+updateResInput = function(){
+	document.getElementById('input').value = myVar;
+};
+cleanLast = function(){
+   
+};
+oneX = function(){
+	Number(myVar)=1/Number(myVar);
+	updateResInput();
+};
+chageSign = function(){
+	Number(myVar)=Number(-myVarl);
+	updateResInput();
 };
