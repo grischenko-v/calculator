84958b1110cc5c81fe240f0aa9ac25acf1dac1b3
Vladimir <boooobka@gmail.com>
2017-09-04 22:16:54 +0400

    add buttons example

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -6,18 +6,27 @@ import Buttom from './Button/Button';
 
 class App extends Component {
   render(){
-       return (    
-       <div>   
-       <Buttom val = "1"/>
-       <Buttom val = "2"/>
-       <Buttom val = "3"/>
-       <Buttom val = "4"/>
-       <Buttom val = "5"/>
-       <Buttom val = "6"/>
-       <Buttom val = "7"/>
-       <Buttom val = "8"/>
-       <Buttom val = "9"/>
-        </div> 
+       return (   
+       <div> 
+       <div className = "buttons-row">   
+         <Buttom val = "mc"/>
+         <Buttom val = "m+"/>
+         <Buttom val = "m-"/>
+         <Buttom val = "mr"/>
+       </div>,
+       <div className = "buttons-row">   
+         <Buttom val = "C"/>
+         <Buttom val = "^"/>
+         <Buttom val = "/"/>
+         <Buttom val = "*"/>
+       </div>,
+       <div className = "buttons-row">   
+         <Buttom val = "7"/>
+         <Buttom val = "8"/>
+         <Buttom val = "9"/>
+         <Buttom val = "-"/>
+       </div>
+      </div>
      );
   }
 }
