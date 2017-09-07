63c38c353b6d805f88b6ca75633269f4a72573ad
Vladimir <boooobka@gmail.com>
2017-09-07 21:50:37 +0400

    chage logic

--- src/mainScreen/MainScreen.js
+++ src/mainScreen/MainScreen.js
@@ -10,19 +10,14 @@ class MainScreen extends Component {
     };
   }
 
-  componentWillReceiveProps(){
-    console.log();
+  render(props) {   
     if(this.props.memLength !== 0) this.textStyle.displayMem = "block";
     else this.textStyle.displayMem = "none";
     if(this.props.resualStr.length >= 0) this.textStyle.fontSize = "50px";
     if(this.props.resualStr.length > 5)  this.textStyle.fontSize = "40px"; 
     if(this.props.resualStr.length > 7)  this.textStyle.fontSize = "30px";
     if(this.props.resualStr.length > 11) this.textStyle.fontSize = "20px";
-    if(this.props.resualStr.length > 18) this.textStyle.fontSize = "15px";
-    
-  }
-
-  render(props) {    
+    if(this.props.resualStr.length > 18) this.textStyle.fontSize = "15px"; 
     return (
     	 <div className = "main-screen" style ={{fontSize: this.textStyle.fontSize}}>{this.props.resualStr} 
     	     <span className =  "mem-sign" style ={{display: this.textStyle.displayMem}}>M</span>
