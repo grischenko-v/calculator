a3b4ece2f67eabe8352ce0e5f56c86a29d20a47c
Vladimir <boooobka@gmail.com>
2017-09-07 21:30:29 +0400

    error corections

--- src/mainScreen/MainScreen.js
+++ src/mainScreen/MainScreen.js
@@ -4,25 +4,25 @@ import './MainScreen.css';
 class MainScreen extends Component {
    constructor(props) {
     super(props);
-    this.state = {
+    this.textStyle = {
       fontSize: "50px",
       displayMem: "none"
-    };     
-  }  
+    };
+  }
+  
   componentWillUpdate(){
-    if(this.props.memLength !== 0) this.state.displayMem = "block";
-    else this.state.displayMem = "none";
-    if(this.props.resualStr.length > 5)  this.state.fontSize = "50px"; 
-    if(this.props.resualStr.length > 7)  this.state.fontSize = "30px";
-    if(this.props.resualStr.length > 12) this.state.fontSize = "20px";
-    if(this.props.resualStr.length > 18) this.state.fontSize = "15px";
+    if(this.props.memLength !== 0) this.textStyle.displayMem = "block";
+    else this.textStyle.displayMem = "none";
+    if(this.props.resualStr.length > 5)  this.textStyle.fontSize = "50px"; 
+    if(this.props.resualStr.length > 7)  this.textStyle.fontSize = "30px";
+    if(this.props.resualStr.length > 12) this.textStyle.fontSize = "20px";
+    if(this.props.resualStr.length > 18) this.textStyle.fontSize = "15px";
   }
 
-  render(props) {     
-    
+  render(props) {    
     return (
-    	 <div className = "main-screen" style ={{fontSize: this.state.fontSize}}>{this.props.resualStr} 
-    	     <span className =  "mem-sign" style ={{display: this.state.displayMem}}>M</span>
+    	 <div className = "main-screen" style ={{fontSize: this.textStyle.fontSize}}>{this.props.resualStr} 
+    	     <span className =  "mem-sign" style ={{display: this.textStyle.displayMem}}>M</span>
     	 </div>     
     );
   }
