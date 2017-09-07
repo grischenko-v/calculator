b8cb2897bef057b28ef15f6773bfcfb524868acd
Vladimir <boooobka@gmail.com>
2017-09-07 21:14:24 +0400

    add componentwillmount state

--- src/mainScreen/MainScreen.js
+++ src/mainScreen/MainScreen.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 import './MainScreen.css';
 
 class MainScreen extends Component {
-  render(props) {  
-    let font = {
-    	fontSize: "70px",
-    	displayMem: "block" 
-    }
-    if(this.props.memLength !== 0) font.displayMem = "block";
-    else font.displayMem = "none";
-    if(this.props.resualStr.length > 5)  font.fontSize = "50px"; 
-    if(this.props.resualStr.length > 7)  font.fontSize = "30px";
-    if(this.props.resualStr.length > 12) font.fontSize = "20px";
-    if(this.props.resualStr.length > 18) font.fontSize = "15px";
+   constructor(props) {
+    super(props);
+    this.state = {
+      fontSize: "50px",
+      displayMem: "none"
+    };     
+  }  
+  componentWillUpdate(){
+    if(this.props.memLength !== 0) this.state.displayMem = "block";
+    else this.state.displayMem = "none";
+    if(this.props.resualStr.length > 5)  this.state.fontSize = "50px"; 
+    if(this.props.resualStr.length > 7)  this.state.fontSize = "30px";
+    if(this.props.resualStr.length > 12) this.state.fontSize = "20px";
+    if(this.props.resualStr.length > 18) this.state.fontSize = "15px";
+  }
+
+  render(props) {     
+    
     return (
-    	 <div className = "main-screen" style ={{fontSize: font.fontSize}}>{this.props.resualStr} 
-    	     <span className =  "mem-sign" style ={{display: font.displayMem}}>M</span>
+    	 <div className = "main-screen" style ={{fontSize: this.state.fontSize}}>{this.props.resualStr} 
+    	     <span className =  "mem-sign" style ={{display: this.state.displayMem}}>M</span>
     	 </div>     
     );
   }
